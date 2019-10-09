93f2eeda58c03601d064579a3580d3e7a354c172
NChristina <christina.stoiber@fhstp.ac.at>
2019-10-09 15:42:36 +0200

    add hint 2 to chart (ref #1)

--- src/js/shared/d3Interaction.js
+++ src/js/shared/d3Interaction.js
@@ -1,12 +1,15 @@
-import { createHintOne } from './d3Manipulations';
+import { createHintOne, createHintTwo } from "./d3Manipulations";
 
 const manageHints = mode => {
-  switch(mode) {
-      case 0:
-          createHintOne();
-          break;
-      default:
-          console.log('Nothing happens');
+  switch (mode) {
+    case 0:
+      createHintOne();
+      break;
+    case 1:
+      createHintTwo();
+      break;
+    default:
+      console.log("Nothing happens");
   }
 };
 
--- src/js/shared/d3Manipulations.js
+++ src/js/shared/d3Manipulations.js
@@ -1,64 +1,125 @@
-import * as d3 from 'd3';
-import { moveElementsToNewParent } from './util';
+import * as d3 from "d3";
+import { moveElementsToNewParent } from "./util";
 
 const createHintOne = () => {
-    const paths = d3.select('.vegaViz1 > svg').selectAll('path');
-    const oneBar = d3.select(paths.nodes()[5]);
-    const oneBarBox = oneBar.node().getBBox();
+  const paths = d3.select(".vegaViz1 > svg").selectAll("path");
+  const oneBar = d3.select(paths.nodes()[5]);
+  const oneBarBox = oneBar.node().getBBox();
 
-    console.log('Bar properties: ', oneBar.node().getBoundingClientRect());
-    console.log(oneBar.node().getBBox());
+  console.log("Bar properties: ", oneBar.node().getBoundingClientRect());
+  console.log(oneBar.node().getBBox());
 
-    const hint1Group = d3
-      .select('.vegaViz1 > svg')
-      .select('.mark-rect')
-      .append('g')
-      .on('click', function(d) {
-        d3.select(this).attr("visibility", "hidden");
-      });
+  const hint1Group = d3
+    .select(".vegaViz1 > svg")
+    .select(".mark-rect")
+    .append("g")
+    .on("click", function(d) {
+      d3.select(this).attr("visibility", "hidden");
+    });
 
-    hint1Group
-      .append('circle')
-      .attr('r', 10)
-      .attr('cx', oneBarBox.x + 10)
-      .attr('cy', oneBarBox.y - 12)
-      .style('stroke', '#C51B7D')
-      .style('fill', '#C51B7D');
-    hint1Group
-      .append('text')
-      .attr('x', oneBarBox.x + 10)
-      .attr('y', oneBarBox.y - 6)
-      .attr('text-anchor', 'middle')
-      .attr('fill', 'white')
-      .text('1');
-      hint1Group.append('rect')
-      .attr('width', oneBarBox.width)
-      .attr('height', oneBarBox.height)
-      .attr('x', oneBarBox.x)
-      .attr('y', oneBarBox.y)
-      .attr('fill', 'none')
-      .attr('stroke', '#C51B7D')
-      .attr('stroke-width', 2);
-
-    console.log('Element: ', d3.select(paths.nodes()[3]));
+  hint1Group
+    .append("circle")
+    .attr("r", 10)
+    .attr("cx", oneBarBox.x + 15)
+    .attr("cy", oneBarBox.y - 12)
+    .style("stroke", "#C51B7D")
+    .style("fill", "#C51B7D");
+  hint1Group
+    .append("text")
+    .attr("x", oneBarBox.x + 15)
+    .attr("y", oneBarBox.y - 6)
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text("1");
+  hint1Group
+    .append("rect")
+    .attr("width", oneBarBox.width)
+    .attr("height", oneBarBox.height)
+    .attr("x", oneBarBox.x)
+    .attr("y", oneBarBox.y)
+    .attr("fill", "none")
+    .attr("stroke", "#C51B7D")
+    .attr("stroke-width", 2);
 
   return 1;
 };
 
-const makeLegendBoxes = () => {
-    d3.select('.stepsContainer')
-    .append('fieldset')
-    .attr('id', 'reading')
-    .append('legend')
-    .text('Reading');
-  d3.select('.stepsContainer')
-    .append('fieldset')
-    .attr('id', 'using')
-    .append('legend')
-    .text('Using');
+const createHintTwo = () => {
+  const paths = d3.select(".vegaViz1 > svg").selectAll("path");
+  const oneBar = d3.select(paths.nodes()[9]);
+  const oneBarBox = oneBar.node().getBBox();
 
-  moveElementsToNewParent(['step-0', 'step-1', 'step-2'], 'reading');
-  moveElementsToNewParent(['step-3', 'step-4', 'step-5'], 'using');
+  console.log("Bar properties: ", oneBar.node().getBoundingClientRect());
+  console.log(oneBar.node().getBBox());
+
+  const hint2Group = d3
+    .select(".vegaViz1 > svg")
+    .select(".mark-rect")
+    .append("g")
+    .on("click", function(d) {
+      d3.select(this).attr("visibility", "hidden");
+    });
+
+  hint2Group
+    .append("circle")
+    .attr("r", 10)
+    .attr("cx", oneBarBox.x + 15)
+    .attr("cy", oneBarBox.y - 12)
+    .style("stroke", "#C51B7D")
+    .style("fill", "#C51B7D");
+  hint2Group
+    .append("text")
+    .attr("x", oneBarBox.x + 15)
+    .attr("y", oneBarBox.y - 6)
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text("2");
+
+  hint2Group
+    .append("line")
+    .attr("x1", oneBarBox.x + oneBarBox.width / 2)
+    .attr("y1", oneBarBox.y)
+    .attr("x2", oneBarBox.x + oneBarBox.width / 2)
+    .attr("y2", oneBarBox.y + oneBarBox.height)
+    .attr("stroke", "#C51B7D")
+    .attr("stroke-width", 2);
+
+  hint2Group
+    .append("line")
+    .attr("x1", oneBarBox.x)
+    .attr("y1", oneBarBox.y)
+    .attr("x2", oneBarBox.x + oneBarBox.width)
+    .attr("y2", oneBarBox.y)
+    .attr("stroke", "#C51B7D")
+    .attr("stroke-width", 2);
+
+  hint2Group
+    .append("line")
+    .attr("x1", oneBarBox.x)
+    .attr("y1", oneBarBox.y + oneBarBox.height)
+    .attr("x2", oneBarBox.x + oneBarBox.width)
+    .attr("y2", oneBarBox.y + oneBarBox.height)
+    .attr("stroke", "#C51B7D")
+    .attr("stroke-width", 2);
+
+  return 2;
 };
 
-export { createHintOne, makeLegendBoxes };
+/** FOR LEGEND BOXES NECESSARY */
+const makeLegendBoxes = () => {
+  d3.select(".stepsContainer")
+    .append("fieldset")
+    .attr("id", "reading")
+    .append("legend")
+    .text("Reading");
+  d3.select(".stepsContainer")
+    .append("fieldset")
+    .attr("id", "using")
+    .append("legend")
+    .text("Using");
+
+  moveElementsToNewParent(["step-0", "step-1", "step-2"], "reading");
+  moveElementsToNewParent(["step-3", "step-4", "step-5"], "using");
+};
+
+export { createHintOne, makeLegendBoxes, createHintTwo };
