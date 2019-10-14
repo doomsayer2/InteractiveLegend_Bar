c6eb825b5d2c31238d53e958d9579987ff8801d0
Florian <fgrassinger@gmx.at>
2019-10-14 10:50:50 +0200

    Remove Tooltip. (refs #1)

--- src/js/components/ModeSwitcher.js
+++ src/js/components/ModeSwitcher.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react';
-import { Switch, Tooltip } from 'antd';
+import { Switch } from 'antd';
 import manageHints, { enableAllHints } from '../shared/d3Interaction';
 import { removeAllHints } from '../shared/d3Manipulations';
 import * as d3 from 'd3';
 
 const ModeSwitcher = ({ mode }) => {
-  const tooltipText =
-    'Toggling the button will show either all hints or let you explore them step by step. However always the current one you have active - stays active';
-
   const toggle = (checked, event, mode) => {
     if (checked) {
       // TODO OTHER IDEA NOW:
@@ -24,7 +21,6 @@ const ModeSwitcher = ({ mode }) => {
 
   return (
     <Fragment>
-      <Tooltip title={tooltipText} arrowPointAtCenter>
         <small style={{ marginRight: 10 + 'px' }}>Show/Hide All Hints</small>
         <Switch
           checkedChildren={'On'}
@@ -34,7 +30,6 @@ const ModeSwitcher = ({ mode }) => {
             toggle(checked, e, mode);
           }}
         ></Switch>
-      </Tooltip>
     </Fragment>
   );
 };
